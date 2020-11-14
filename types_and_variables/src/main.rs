ee6750634e4df54f49e2ac39c88c22039c07b6db
banuAndrei99 <49089603+banuAndrei99@users.noreply.github.com>
2020-11-14 13:01:18 +0200

    types and variables
    played with core types in order to get a better understanding of them

---
+++ types_and_variables/src/main.rs
@@ -0,0 +1,44 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use std::mem;
+
+
+fn main() {
+    let a: u8 = 123;  // u = unsigned, 8 bits --> 0 - 255
+    println!("a = {}", a);
+    // a = 456;  let bidding => in immutable variables, all variables are immutable by default
+
+
+    // u = unsigned, 0 --> 2^(N-1)
+    // i = signed, -2^(N-1) --> 2^(N-1)-1
+    let mut b: i8 = 123; // mut keyword will explicitly make the variable mutable
+    println!("b = {} before", b);
+    b = -4;
+    println!("b = {} after", b);
+
+    let c = 123456789; // type inferred --> i32
+    println!("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
+
+    // u8, u16, u32, u64, i8, i16, ...
+
+    // usize isize --> size of processor bitness
+
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z);  // type inferred --> usize
+    println!("z = {}, takes up {}, {}-bit OS", z, size_of_z, size_of_z * 8);
+
+    let d: char = 'x';
+    println!("d = {} is a char, takes up {} bytes", d, mem::size_of_val(&d));
+
+    // f32, f64 --> standardize(IEEE754) floating point numbers
+    // f32, f64 --> all signed!
+
+    let e = 2.5; // type inferred to f64(default type for non-whole numbers)
+    println!("e = {}, takes up {} bytes", e, mem::size_of_val(&e));
+
+    let g = false; // type inferred to bool
+    println!("g = {}, takes up {} bytes", g, mem::size_of_val(&g));
+
+}
